624c35442632d7424c5d64a336182a01c24f6b97
K1auez <skaue3222@gmail.com>
2024-09-02 15:19:14 -0300

    a

--- back-end/cadastro_processo.php
+++ back-end/cadastro_processo.php
@@ -1,17 +1,26 @@
 <?php
 include '../conexao.php';
 
+// Obtendo os dados do formulário
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-$senha = md5($_POST['senha']); // Criptografe a senha para segurança
+$senha = $_POST['senha']; // Captura a senha do formulário
 
-$sql = "INSERT INTO cadastro (cadastro_nome, cadastro_email, cadastro_senha) VALUES ('$nome', '$email', '$senha')";
+// Criptografando a senha com password_hash()
+$senha_hash = password_hash($senha, PASSWORD_BCRYPT); // Utiliza bcrypt para o hash da senha
 
-if ($conexao->query($sql) === TRUE) {
+// Preparando a query SQL com prepared statements para evitar SQL Injection
+$sql = $conexao->prepare("INSERT INTO cadastro (cadastro_nome, cadastro_email, cadastro_senha) VALUES (?, ?, ?)");
+$sql->bind_param("sss", $nome, $email, $senha_hash);
+
+// Executa a query
+if ($sql->execute()) {
     header("Location: ../login.php"); // Redireciona para a página de login
 } else {
-    echo "Erro: " . $sql . "<br>" . $conexao->error;
+    echo "Erro: " . $sql->error;
 }
 
+// Fecha a conexão
+$sql->close();
 $conexao->close();
 ?>
--- back-end/login_processo.php
+++ back-end/login_processo.php
@@ -3,27 +3,33 @@ session_start();
 include '../conexao.php';
 
 $email = $_POST['username']; 
-$senha = md5($_POST['password']);
+$senha = $_POST['password'];
+
+// Consulta para verificar se o usuário existe
+$sql = "SELECT * FROM cadastro WHERE cadastro_email = ?";
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-    $_SESSION["loggedin"] = true;
-    header("Location: ../index_adm.php");
-} else {
-    // Consulta para verificar se o usuário é um usuário regular
-    $sql = "SELECT * FROM cadastro WHERE cadastro_email = '$email' AND cadastro_senha = '$senha'";
-    $result = $conexao->query($sql);
+    $row = $result->fetch_assoc();
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
+    // Verifica se a senha está correta
+    if (password_verify($senha, $row['cadastro_senha'])) {
         $_SESSION["loggedin"] = true;
         $_SESSION['usuario_id'] = $row['cadastro_id'];
- 
+
         header("Location: ../index.php");
     } else {
         print "<script>alert('Email ou senha incorretos');</script>";
         print "<script>location.href='../login.php';</script>";
     }
+} else {
+    print "<script>alert('Email ou senha incorretos');</script>";
+    print "<script>location.href='../login.php';</script>";
 }
 
+$stmt->close();
 $conexao->close();
 ?>
